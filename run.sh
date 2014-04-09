95ec73d6092682afef7ce0302281120741744e4f
android <android@honeynet.(none)>
2014-04-09 11:36:19 -0400

    comments

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 
 
 # mode the date
-python scripts/mod_date.py
+#python scripts/mod_date.py
 
 # split input into train set and test set
-python scripts/split.py
+#python scripts/split.py
 
 # gen prediction output
 python scripts/gen_output.py
 
 # local test
-python scripts/test.py
+#python scripts/test.py
